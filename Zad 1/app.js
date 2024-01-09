615d51b2737499a2a57710cac0e6e1df1f4b2ed5
Michał Soja <m.sojka99@gmail.com>
2024-01-09 22:55:49 +0100

    quick fix in zad 1 (port)

--- Zad 1/app.js
+++ Zad 1/app.js
@@ -137,7 +137,7 @@ app.delete("/cars/:number", async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
