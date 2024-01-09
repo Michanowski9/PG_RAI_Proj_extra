d1798c483e97f1c3bebd3223db61140052b6f86f
Michał Soja <m.sojka99@gmail.com>
2024-01-09 22:22:28 +0100

    zad 5 comments

--- Zad 5/server.js
+++ Zad 5/server.js
@@ -1,11 +1,11 @@
-const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: 3000 });
+const WebSocket = require('ws'); //  Importuje bibliotekę ws, która umożliwia obsługę WebSocket na serwerze.
+const server = new WebSocket.Server({ port: 3000 }); // Tworzy serwer WebSocket nasłuchujący na porcie 3000.
 
-server.on('connection', (socket) => {
+server.on('connection', (socket) => { // Nasłuchuje na zdarzenie nawiązania nowego połączenia i wykonuje funkcję zwrotną, która otrzymuje obiekt reprezentujący to połączenie (socket).
   console.log('Nowe połączenie WebSocket.');
 
   // Nasłuchuj wiadomości od klienta
-  socket.on('message', (message) => {
+  socket.on('message', (message) => { // Nasłuchuje na zdarzenie otrzymania wiadomości od klienta i wykonuje funkcję zwrotną, która wypisuje wiadomość na konsoli i wysyła odpowiedź do klienta.
     console.log(`Otrzymano wiadomość: ${message}`);
 
     // Odpowiedz na wiadomość od klienta
@@ -13,5 +13,5 @@ server.on('connection', (socket) => {
   });
 
   // Wyślij przywitalną wiadomość do klienta po nawiązaniu połączenia
-  socket.send('Witaj na serwerze WebSocket!');
+  socket.send('Witaj na serwerze WebSocket!'); // Wysyła przywitalną wiadomość do klienta po nawiązaniu połączenia.
 });
